4e81efa935c944c16bfab99b7bf6ca58354d949c
Kent Daleng <kent.daleng@nav.no>
2020-11-30 23:09:06 +0100

    add test files

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ fn main() -> Result<()> {
 }
 
 #[test]
-fn lines_from_99_should_be_1_2_3() {
-    assert_eq!(vec![1, 2, 3], lines_from!("99", u32))
+fn lines_from_test1_should_be_1_2_3() {
+    assert_eq!(vec![1, 2, 3], lines_from!("test1", u32))
 }
 
 #[test]
-fn csv_from_100_should_be_65535_65536() {
-    assert_eq!(vec![65535, 65536], csv_from!("100", u32))
+fn csv_from_test2_should_be_65535_65536() {
+    assert_eq!(vec![65535, 65536], csv_from!("test2", u32))
 }

ac4deba8b7ba82d70894c9d183724c6d5db6f06f
Kent Daleng <kent.daleng@nav.no>
2020-12-02 21:16:36 +0100

    slightly less spaghetti

--- src/days/day2/mod.rs
+++ src/days/day2/mod.rs
@@ -2,8 +2,8 @@ pub mod parser;
 
 fn solve_one(entries: &Vec<Password>) -> u32 {
     entries.iter().fold(0, |count, pw| {
-        let matches = pw.3.matches(pw.2).count();
-        if matches >= pw.0 && matches <= pw.1 {
+        let matches = pw.password.matches(pw.target).count();
+        if matches >= pw.pos1 && matches <= pw.pos2 {
             count + 1
         } else {
             count
@@ -13,10 +13,10 @@ fn solve_one(entries: &Vec<Password>) -> u32 {
 
 fn solve_two(entries: &Vec<Password>) -> u32 {
     entries.iter().fold(0, |count, pw| {
-        let first = pw.3.chars().nth(pw.0-1).unwrap();
-        let second = pw.3.chars().nth(pw.1-1).unwrap();
-        if (first == pw.2 && second == pw.2) 
-        || (first != pw.2 && second != pw.2) {
+        let first = pw.password.chars().nth(pw.pos1-1).unwrap();
+        let second = pw.password.chars().nth(pw.pos2-1).unwrap();
+        if (first == pw.target && second == pw.target)
+        || (first != pw.target && second != pw.target) {
             count
         } else {
             count + 1
@@ -35,7 +35,12 @@ pub fn two(entries: &Vec<Password>) -> String {
     format!("Day 2-2:  {}", answer)
 }
 
-pub type Password = (usize, usize, char, String);
+pub struct Password {
+    pos1: usize,
+    pos2: usize,
+    target: char,
+    password: String
+}
 
 #[test]
 fn regression() {
--- src/days/day2/parser.rs
+++ src/days/day2/parser.rs
@@ -12,17 +12,16 @@ pub fn parse(filename: &str) -> Vec<Password> {
 
 fn process(line: String) -> Password {
     // friendship ended with regex, now spaghetti is my best friend
-    let mut splits = line.split(": ");
-    let rule = splits.nth(0).unwrap().to_string();
-    let password = splits.nth(0).unwrap().to_string();
-    let mut positions = rule.split("-");
-    let pos1: usize = positions.nth(0).unwrap().parse().unwrap();
-    let pos2: usize = positions.nth(0).unwrap().split(" ").nth(0).unwrap().parse().unwrap();
-    let target: char = rule.split(" ").nth(1).unwrap().chars().nth(0).unwrap();
-    (pos1, pos2, target, password)
+    let mut line_iter = line.chars();
+    Password {
+        pos1: line_iter.by_ref().take_while(|c| c != &'-').collect::<String>().parse().unwrap(),
+        pos2: line_iter.by_ref().take_while(|c| c != &' ').collect::<String>().parse().unwrap(),
+        target: line_iter.by_ref().nth(0).unwrap(),
+        password: line_iter.skip(2).collect::<String>().to_string()
+    }
 }
 
 #[test]
 fn parse_test() {
-    assert_eq!(String::from("abcde"), parse("test")[0].3)
+    assert_eq!("abcde", parse("test")[0].password)
 }

c9632b25bbd0001f7d436a0cf235aa62ec313391
Kent Daleng <kent.daleng@nav.no>
2020-12-03 22:57:05 +0100

    read bytes instead of lines

--- src/days/day3/parser.rs
+++ src/days/day3/parser.rs
@@ -1,15 +1,19 @@
 use super::{Terrain, Field};
-use std::io::BufRead;
+use std::io::Read;
 
 pub fn parse(filename: &str) -> Field {
-    let file = std::fs::File::open(format!("src/days/day3/resources/{}.txt", filename)).unwrap();
-    let reader = std::io::BufReader::new(file);
-    reader.lines().map(|val| val.unwrap().bytes().map(|c| {
-        if c == 35 { Terrain::Tree } else { Terrain::Nothing }
+    let mut file = std::fs::File::open(format!("src/days/day3/resources/{}.txt", filename)).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    buffer.split(|b| b < &32).filter(|slice| slice.len() > 0).map(|val| val.iter().map(|b| {
+        if b == &35 { Terrain::Tree } else { Terrain::Nothing }
     }).collect::<Vec<Terrain>>()).collect::<Field>()
 }
 
 #[test]
 fn parse_test() {
-    assert_eq!(Terrain::Nothing, parse("test")[0][0])
+    let field = parse("test");
+    assert_eq!(Terrain::Nothing, field[0][0]);
+    assert_eq!(Terrain::Tree, field[1][0]);
+
 }

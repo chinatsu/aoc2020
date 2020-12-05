4dfe6f0fb8a8b21f70fc7a259e4f399c78bd30cf
Kent Daleng <kent.daleng@nav.no>
2020-12-05 10:22:14 +0100

    just do stuff with seat ids

--- src/days/day5/mod.rs
+++ src/days/day5/mod.rs
@@ -3,48 +3,27 @@ pub mod parser;
 pub const TEST: &[u8] = include_bytes!("resources/test.txt");
 pub const INPUT: &[u8] = include_bytes!("resources/input.txt");
 
-fn solve_one(input: &Vec<BoardingPass>) -> u32 {
-    input.iter().fold(0, |champion, challenger| {
-        if challenger.seat_id > champion {
-            challenger.seat_id
-        } else {
-            champion
-        }
-    })
+fn solve_one(input: &Vec<u32>) -> u32 {
+    input.iter().fold(0, |champion, challenger| std::cmp::max(*challenger, champion))
 }
 
-fn solve_two(input: &Vec<BoardingPass>) -> u32 {
-    let ids: Vec<u32> = input.iter().map(|pass| pass.seat_id).collect();
-    let mut first_seat_encountered = false;
-    for r in 0..=8096 {
-        if ids.contains(&r) {
-            if !first_seat_encountered {
-                first_seat_encountered = true;
-            }
-        } else {
-            if first_seat_encountered && ids.contains(&(r+1)) && ids.contains(&(r-1)) { 
-                return r
-            }
-        }
-    }
-    0
+fn solve_two(input: &Vec<u32>) -> Option<u32> {
+    (*input.iter().min().unwrap()..*input.iter().max().unwrap())
+        .filter(|r| !input.contains(&r) && input.contains(&(r-1)) && input.contains(&(r+1)))
+        .next()
 }
 
-pub fn one(input: &Vec<BoardingPass>) -> String {
+pub fn one(input: &Vec<u32>) -> String {
     let answer = solve_one(input);
     format!("Day 5-1:  {}", answer)
 }
 
-pub fn two(input: &Vec<BoardingPass>) -> String {
+pub fn two(input: &Vec<u32>) -> String {
     let answer = solve_two(input);
-    format!("Day 5-2:  {}", answer)
-}
-
-
-pub struct BoardingPass {
-    pub row: usize,
-    pub column: usize,
-    pub seat_id: u32
+    match answer {
+        Some(a) => format!("Day 5-2:  {}", a),
+        None => String::from("Day 5-2:  Something's off..")
+    }
 }
 
 #[test]
@@ -54,13 +33,12 @@ pub fn solve_one_test() {
 
 #[test]
 pub fn solve_two_test() {
-    // there is no seat to be found with the test set
-    assert_eq!(0, solve_two(&parser::parse(TEST)))
+    assert_eq!(None, solve_two(&parser::parse(TEST)))
 }
 
 #[test]
 pub fn regression() {
     let input = parser::parse(INPUT);
     assert_eq!(989, solve_one(&input));
-    assert_eq!(548, solve_two(&input));
+    assert_eq!(Some(548), solve_two(&input));
 }
--- src/days/day5/parser.rs
+++ src/days/day5/parser.rs
@@ -1,42 +1,11 @@
-use super::BoardingPass;
-
-pub fn parse(content: &[u8]) -> Vec<BoardingPass> {
+pub fn parse(content: &[u8]) -> Vec<u32> {
     content.split(|b| *b == 10).filter(|pass| pass.len() > 0).map(|pass| {
-        process_boarding(pass)
-    }).collect::<Vec<BoardingPass>>()
-}
-
-fn process_boarding(input: &[u8]) -> BoardingPass {
-    let mut row: Vec<usize> = (0..=127).collect();
-    let mut column: Vec<usize> = (0..=7).collect();
-    for b in input.iter() {
-        if *b == 70 {
-            row = take(row, Side::Bottom);
-        } else if *b == 66 {
-            row = take(row, Side::Top);
-        } else if *b == 76 {
-            column = take(column, Side::Bottom);
-        } else if *b == 82 {
-            column = take(column, Side::Top);
-        }
-    }
-    BoardingPass {
-        row: row[0],
-        column: column[0],
-        seat_id: row[0] as u32 * 8 + column[0] as u32
-    }
-}
-
-enum Side {
-    Top, Bottom
-}
-
-fn take(input: Vec<usize>, side: Side) -> Vec<usize> {
-    let middle = input.len()/2;
-    match side {
-        Side::Top => input.split_at(middle).1.to_vec(),
-        Side::Bottom => input.split_at(middle).0.to_vec()   
-    }
+        pass.iter().fold(0, |n, b| match b {
+            66 | 82 => 2 * n + 1,
+            70 | 76 => 2 * n,
+            _ => n
+        })
+    }).collect::<Vec<u32>>()
 }
 
 #[test]
@@ -45,11 +14,6 @@ pub fn parse_test() {
     let passes = parse(TEST);
     assert_eq!(4, passes.len());
 
-    assert_eq!(44, passes[0].row);
-    assert_eq!(5, passes[0].column);
-    assert_eq!(357, passes[0].seat_id);
-
-    assert_eq!(102, passes[3].row);
-    assert_eq!(4, passes[3].column);
-    assert_eq!(820, passes[3].seat_id);
+    let expected = [357, 567, 119, 820].to_vec();
+    assert_eq!(expected, passes);
 }

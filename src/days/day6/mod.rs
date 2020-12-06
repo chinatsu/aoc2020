f2331e0bcd12f13f442f50750de6e7596fdcefa5
Kent Daleng <kent.daleng@nav.no>
2020-12-06 12:02:27 +0100

    make the algo at least semantically nicer

--- src/days/day6/mod.rs
+++ src/days/day6/mod.rs
@@ -1,35 +1,29 @@
 pub mod parser;
-use std::collections::HashSet;
 
 pub const TEST: &str = include_str!("resources/test.txt");
 pub const INPUT: &str = include_str!("resources/input.txt");
 
 fn solve_one(input: &Vec<Vec<&str>>) -> usize {
-    input.iter().fold(0, |acc, element| {
-        acc + element.join("").chars().collect::<HashSet<char>>().len()
+    input.iter().fold(0, |acc, group| {
+        acc + char_set(group).len()
     })
 }
 
-fn solve_two(input: &Vec<Vec<&str>>) -> usize {
-    // big gross
-    let mut sum = 0;
-    input.iter().for_each(|set| {
-        if set.len() == 1 {
-            sum += set[0].chars().collect::<HashSet<char>>().len();
-        } else {
-            let answers = set.iter()
-                .map(|answer| {
-                    answer.chars().collect::<HashSet<char>>()
-                })
-                .collect::<Vec<HashSet<char>>>();
-            let mut common: HashSet<char> = answers[0].clone();
-            answers.iter().for_each(|answer| {
-                common = common.intersection(answer).cloned().collect::<HashSet<char>>();
-            });
-            sum += common.len();
-        }
-    });
-    sum
+fn solve_two(input: &Vec<Vec<&str>>) -> u32 {
+    input.iter().map(|group| {
+        char_set(group)
+            .iter()
+            .fold(0, |acc, c| acc + group
+                .iter()
+                .all(|person| person.contains(&c.to_string())) as u32)
+    }).sum()
+}
+
+fn char_set(input: &Vec<&str>) -> Vec<char> {
+    let mut ret: Vec<char> = input.join("").chars().collect();
+    ret.sort();
+    ret.dedup();
+    ret
 }
 
 pub fn one(input: &Vec<Vec<&str>>) -> String {
